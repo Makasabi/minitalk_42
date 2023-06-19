1c0fbaebee71c782809e4dfde61d43155f0f2917
Marie RONY <mrony@bess-f3r5s6.clusters.42paris.fr>
2023-06-19 12:17:54 +0200

    commented SIGUSR1 message client

--- client_docs/sources/client.c
+++ client_docs/sources/client.c
@@ -6,7 +6,7 @@
 /*   By: mrony <mrony@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:28:13 by mrony             #+#    #+#             */
-/*   Updated: 2023/06/14 18:15:25 by mrony            ###   ########.fr       */
+/*   Updated: 2023/06/19 12:17:28 by mrony            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,10 @@ void	ft_client_handler(int signal, siginfo_t *info, void *context)
 	(void)context;
 	if (signal == SIGUSR1 && flag == 0)
 	{
-		ft_printf("\033[0;34m.\033[1;0m");
+		//ft_printf("\033[0;34m.\033[1;0m");
 	}
 	if (signal == SIGUSR2)
-	{
-		ft_printf("\033[0;34mString Status: \033[1;0mReceived 🦚\n");
-		flag = 1;
-	}
-	g_flag = 1;
-}
+	{git@github.com:Makasabi/philosophers_42.git
 
 void	ft_int_to_bin(int size, int pid)
 {
